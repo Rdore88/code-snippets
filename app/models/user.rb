7195aa68886dcfe4717f9a168dfdc3ccf73abd37
Robby Dore <rdore88@gmail.com>
2017-10-02 16:41:35 -0400

    Created user model with validations, created empty container components

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :password, presence: true
+  validates :username, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: true
+
+
+
+end
