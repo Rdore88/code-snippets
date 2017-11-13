ba29ce67195556cfb081d335b50f5d54a2fe9e67
Robby Dore <rdore88@gmail.com>
2017-11-13 12:53:29 -0500

    seeded database with two users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'robby', email: 'robby@dore.com', password: 'robby', password_confirmation: 'robby', authorization_token: SecureRandom.hex(64))
+User.create(username: 'hello', email: 'hello@hello.com', password: 'hello', password_confirmation: 'hello', authorization_token: SecureRandom.hex(64))
